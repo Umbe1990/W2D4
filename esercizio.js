8fd3fbb20d753b37f0f78126870dc4ce9a436762
umberto conedera <161523775+Umbe1990@users.noreply.github.com>
2024-03-24 19:55:23 +0100

    inizio

---
+++ esercizio.js
@@ -0,0 +1,27 @@
+const prices = [34, 5, 2]
+let somma=0
+for(let i=0; i<prices.length; i++){
+
+    somma+=prices[i]
+}
+console.log("la somma dell' arrey è " + somma)
+
+
+
+const isAmbassador= true
+if(isAmbassador){
+    const sconto= somma *0.30;
+    const sommaScontata= somma-sconto
+    const arrotondamento=sommaScontata.toFixed(2)
+    console.log("Utente con  valore isAmbassador true paga " + arrotondamento)
+}
+
+const shippingCost = 50
+const arrotondamento=28.70
+
+if(arrotondamento<100){
+    importoFinale= arrotondamento + shippingCost
+    console.log(importoFinale)
+}
+
+
